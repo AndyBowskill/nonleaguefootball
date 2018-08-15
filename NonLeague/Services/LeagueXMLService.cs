6b646280e33ee54965fb1511b38530250290d344
Andy Bowskill <andybowskill@gmail.com>
2018-08-15 19:52:06 +0100

    Changes to favicon and tidy up services code.

--- NonLeague/Services/LeagueXMLService.cs
+++ NonLeague/Services/LeagueXMLService.cs
@@ -12,10 +12,9 @@ namespace NonLeague.Services
     {
         private List<League> leagueList;
          
-        public IEnumerable<League> GetAll(string webRoot)
+        public IEnumerable<League> GetAll(string webRoot) 
         {
-            var leagues = from element in XDocument.Load(webRoot + "/xml/leagues.xml").Descendants("league") select element;
-
+            var leagues = GetLeaguesFromXML(webRoot);
             leagueList = new List<League>();
             
             foreach (var item in leagues)
@@ -30,23 +29,34 @@ namespace NonLeague.Services
             }
             
             return leagueList;
-            
         }
-        
+
         public string GetDescription(int competitionID, string webRoot)
         {
-            // ToDo - Prevent exception if user uses URL to ask for a competition that doesn't exist (e.g. 1,2,3,etc.)
-            
-            if (leagueList == null)
-            {
-                leagueList = GetAll(webRoot).ToList();
-            }
-            
-            League league = leagueList.First(item => item.CompetitionID == competitionID);            
-            
-            return string.Format("{0} - {1}", league.Name, league.Division);
+            var leagues = GetLeaguesFromXML(webRoot);
+            string leagueDescription = null;
 
+            foreach (var item in leagues)
+            {
+                if (competitionID == Convert.ToInt32(item.Attribute("competitionID").Value))
+                {
+                   leagueDescription = string.Format("{0} - {1}", item.Attribute("name").Value, item.Attribute("division").Value);
+                   break;
+                }
+         
+            }
+
+            if (leagueDescription == null)
+            {
+                leagueDescription = "N/A";
+            }
+
+            return leagueDescription;
+        }
+
+        private IEnumerable<XElement> GetLeaguesFromXML(string webRoot)
+        {
+            return from element in XDocument.Load(webRoot + "/xml/leagues.xml").Descendants("league") select element;
         }
     }
-    
 }
